99d0d7723c2f216b5adc7e7ed10e92bbcb1b1718
DiegoAedo <diego.aedoqmail.com>
2022-07-03 23:31:20 -0400

    Update6

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,24 +1,15 @@
-package com.platinum.login.test;
-
 import static org.junit.Assert.*;
+
 import org.junit.Test;
-
+import com.platinum.login.database.JDBCDataSource;
 import java.sql.Connection;
+public class test {
 
-
-
-
-
-import com.platinum.login.database.*;
-
-
-public class TestLogin {
-	
-	@Test
-	public void getConnectionTest() throws Exception {
+	@Test 
+    public void getConnectionTest() throws Exception {
         Connection dbConnection = (Connection) JDBCDataSource.getConnection();
         assertNotNull("connection should be successfull.", dbConnection);
  
-    }
-	
+	}
+
 }
