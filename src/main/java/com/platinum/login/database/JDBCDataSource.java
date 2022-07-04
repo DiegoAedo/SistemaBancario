e464f8725e327def3bfa61424796bb76b6b695b7
DiegoAedo <diego.aedoqmail.com>
2022-07-03 23:13:12 -0400

    Update4

--- src/main/java/com/platinum/login/database/JDBCDataSource.java
+++ src/main/java/com/platinum/login/database/JDBCDataSource.java
@@ -2,12 +2,10 @@ package com.platinum.login.database;
 
 import java.sql.Connection;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
-import java.util.Properties;
+
 public class JDBCDataSource {
 	
     private static JDBCDataSource datasource;
-    private static Properties prop = null;
-    
     private JDBCDataSource() {
     }
     private ComboPooledDataSource cpds = null;
@@ -25,10 +23,7 @@ public class JDBCDataSource {
             datasource.cpds.setJdbcUrl("jdbc:mysql://localhost:3306/login");
             datasource.cpds.setUser("root");
             datasource.cpds.setPassword("root");
-            datasource.cpds.setInitialPoolSize(10);
-            datasource.cpds.setAcquireIncrement(10);
-            datasource.cpds.setMaxPoolSize(100);
-            datasource.cpds.setMinPoolSize(10);
+
         }
         return datasource;
     }
--- src/main/java/com/platinum/login/test/TestLogin.java
+++ src/main/java/com/platinum/login/test/TestLogin.java
@@ -3,18 +3,12 @@ package com.platinum.login.test;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import com.platinum.login.bean.LoginBean;
+
+
+
 import com.platinum.login.database.*;
 
 
