c878ef4e5596413537b5865d3304ae2dcf1cf5f2
DiegoAedo <diego.aedoqmail.com>
2022-07-03 22:06:31 -0400

    Updtate

---
+++ src/main/java/TestLogin.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.junit.Test;
+import com.platinum.login.bean.LoginBean;
+
+public class TestLogin {
+
+	@Test
+	
+	public void test() {
+	
+		
+	}
+			
+
+}
