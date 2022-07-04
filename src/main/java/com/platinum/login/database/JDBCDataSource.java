45b7ccc27d1f6e0d8ad0d84edefc27adfc95f127
DiegoAedo <diego.aedoqmail.com>
2022-07-04 02:28:25 -0400

    Updtate9

--- src/main/java/com/platinum/login/database/JDBCDataSource.java
+++ src/main/java/com/platinum/login/database/JDBCDataSource.java
@@ -1,11 +1,14 @@
 package com.platinum.login.database;
 
 import java.sql.Connection;
+import java.util.Properties;
+
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class JDBCDataSource {
 	
     private static JDBCDataSource datasource;
+    private static Properties prop = null;
     private JDBCDataSource() {
     }
     private ComboPooledDataSource cpds = null;
@@ -23,7 +26,11 @@ public class JDBCDataSource {
             datasource.cpds.setJdbcUrl("jdbc:mysql://localhost:3306/login");
             datasource.cpds.setUser("root");
             datasource.cpds.setPassword("root");
-
+            datasource.cpds.setInitialPoolSize(10);
+            datasource.cpds.setAcquireIncrement(10);
+            datasource.cpds.setMaxPoolSize(100);
+            datasource.cpds.setMaxIdleTime(DataUtility.getInt("100"));
+            datasource.cpds.setMinPoolSize(10);
         }
         return datasource;
     }
